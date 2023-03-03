8fdd7debb6283e1aa4c80a85a5519898f1f11d16
BrunoRijo <bruno_rijo@hotmail.com>
2023-03-03 12:38:34 -0300

    ciclo de vida

---
+++ fundamentos-angular/src/app/ciclo-de-vida/ciclo-de-vida.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, SimpleChange } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo-de-vida',
+  templateUrl: './ciclo-de-vida.component.html',
+  styleUrls: ['./ciclo-de-vida.component.css']
+})
+
+export class CicloDeVidaComponent {
+  horario = new Date();
+  timer : any = null;
+
+  @Input() texto = "";
+
+  ngOnInit(): void{
+    console.log('O evento onInit disparou');
+    this.timer = setInterval(() => this.horario = new Date(), 1000);
+  }
+
+  ngOnChanges(changes: SimpleChange): void {
+    console.log(changes);
+  }
+}
