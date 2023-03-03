c159991f01ee731926f4341b5a4548f58b8f089c
BrunoRijo <bruno_rijo@hotmail.com>
2023-03-03 12:20:01 -0300

    Aprendendo a utilizar Serviços

---
+++ fundamentos-angular/src/app/logger.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class LoggerService {
+  mensagens : string[] = [];
+
+  logar(mensagem:string) { 
+    alert(mensagem);
+    this.mensagens.push(mensagem);
+  }
+
+  exibeTodosOsLogs(){
+    console.log(this.mensagens);
+  }
+}
