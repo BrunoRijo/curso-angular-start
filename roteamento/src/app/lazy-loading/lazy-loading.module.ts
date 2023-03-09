23d9f55b3f2718fdc5c1092d16afe111ff90cd1f
BrunoRijo <bruno_rijo@hotmail.com>
2023-03-08 21:44:09 -0300

    Lazy-loading

--- roteamento/src/app/lazy-loading/lazy-loading.module.ts
+++ roteamento/src/app/lazy-loading/lazy-loading.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { RouterModule, Routes } from '@angular/router';
 import { LazyLoadingRoutingModule } from './lazy-loading-routing.module';
 import { LazyLoadingComponent } from './lazy-loading.component';
 
+const routes : Routes =[{ path: '', component: LazyLoadingComponent}];
 
 @NgModule({
-  declarations: [
-    LazyLoadingComponent
-  ],
-  imports: [
-    CommonModule,
-    LazyLoadingRoutingModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
+
 export class LazyLoadingModule { }
